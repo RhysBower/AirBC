a77437281f9577fa82cfa013d02b6ce6d78dcf8b
Rhys Bower <bowerrhys@gmail.com>
2017-03-11 09:33:42 -0800

    Convert project to PHP 7.0 to support MAMP on windows
    Null return types are not supported and were removed.

--- project-final/src/Airbc/Database.php
+++ project-final/src/Airbc/Database.php
@@ -27,7 +27,7 @@ class Database extends Object
     /**
      * Returns Account with given id or null if no Account exists.
      */
-    public function getAccount(int $id): ?Model\Account
+    public function getAccount(int $id): Model\Account
     {
         if ($result = $this->mysqli->query("SELECT * FROM Account WHERE id=$id")) {
             $this->logger->info("SELECT Account returned $result->num_rows rows");
@@ -77,7 +77,7 @@ class Database extends Object
     /**
      * Returns Account with given id or null if no Account exists.
      */
-    public function getRoute(string $departure, string $arrival): ?Model\Route
+    public function getRoute(string $departure, string $arrival): Model\Route
     {
         if ($result = $this->mysqli->query("SELECT * FROM Route WHERE departure=$departure AND arrival=$arrival")) {
             $this->logger->info("SELECT Route returned $result->num_rows rows");
