b8f67d26f1492afcae6d20b98c46571e77adf4b2
Rhys Bower <bowerrhys@gmail.com>
2017-03-21 22:52:40 -0700

    Convert Database to static class.
    Now access the database with Database:: from anywhere in the
    application.

--- project-final/src/Airbc/Database.php
+++ project-final/src/Airbc/Database.php
@@ -11,27 +11,27 @@ use Airbc\MySQL\MySQLException;
  */
 class Database extends Object
 {
-    private $mysql;
+    private static $mysql;
 
     public function __construct()
     {
         try {
-            $this->mysql = new MySQL('localhost', 'root', 'root', 'cpsc304');
-            $this->mysql->connect();
+            self::$mysql = new MySQL('localhost', 'root', 'root', 'cpsc304');
+            self::$mysql->connect();
         } catch (MySQLException $e) {
             Log::emergency('Connect Error (' . $e->getCode() . ') ' . $e->getMessage());
             die('Connect Error (' . $e->getCode() . ') ' . $e->getMessage());
         }
 
-        Log::info('Connected to MySQL: ' . $this->mysql->hostInfo());
+        Log::info('Connected to MySQL: ' . self::$mysql->hostInfo());
     }
 
     /**
      * Returns Account with given id or null if no Account exists.
      */
-    public function getAccount(int $id): ?Model\Account
+    public static function getAccount(int $id): ?Model\Account
     {
-        return $this->querySingle("SELECT * FROM Account WHERE id=$id", function($row) {
+        return self::querySingle("SELECT * FROM Account WHERE id=$id", function($row) {
             return new Model\Account((int)$row->id, $row->name, $row->email, $row->username, $row->password);
         });
     }
@@ -39,9 +39,9 @@ class Database extends Object
     /**
      * Returns Account with given username or null if no Account exists.
      */
-    public function getUserAccount(string $username): ?Model\Account
+    public static function getUserAccount(string $username): ?Model\Account
     {
-        return $this->querySingle("SELECT * FROM Account WHERE username='$username'", function($row) {
+        return self::querySingle("SELECT * FROM Account WHERE username='$username'", function($row) {
             return new Model\Account((int)$row->id, $row->name, $row->email, $row->username, $row->password);
         });
     }
@@ -49,9 +49,9 @@ class Database extends Object
     /**
      * Returns array of Account or empty array if no Accounts are found.
      */
-    public function getAccounts(): array
+    public static function getAccounts(): array
     {
-        return $this->queryMultiple("SELECT * FROM Account", function($row) {
+        return self::queryMultiple("SELECT * FROM Account", function($row) {
             return new Model\Account((int)$row->id, $row->name, $row->email, $row->username, $row->password);
         });
     }
@@ -59,33 +59,33 @@ class Database extends Object
     /**
      * Returns true if account with given id is a customer, false otherwise.
      */
-    public function isCustomer(int $id): bool
+    public static function isCustomer(int $id): bool
     {
-        return $this->isAccount("SELECT * FROM Customer WHERE id=$id");
+        return self::isAccount("SELECT * FROM Customer WHERE id=$id");
     }
 
     /**
      * Returns true if account with given id is a loyalty member, false otherwise.
      */
-    public function isLoyaltyMember(int $id): bool
+    public static function isLoyaltyMember(int $id): bool
     {
-        return $this->isAccount("SELECT * FROM Loyalty_Member WHERE id=$id");
+        return self::isAccount("SELECT * FROM Loyalty_Member WHERE id=$id");
     }
 
     /**
      * Returns true if account with given id is a staff, false otherwise.
      */
-    public function isStaff(int $id): bool
+    public static function isStaff(int $id): bool
     {
-        return $this->isAccount("SELECT * FROM Staff WHERE id=$id");
+        return self::isAccount("SELECT * FROM Staff WHERE id=$id");
     }
 
     /**
      * Returns a Route from departure airport to arrival airport.
      */
-    public function getRoute(string $departure, string $arrival): ?Model\Route
+    public static function getRoute(string $departure, string $arrival): ?Model\Route
     {
-        return $this->querySingle("SELECT * FROM Route WHERE departure='$departure' AND arrival='$arrival'", function($row) {
+        return self::querySingle("SELECT * FROM Route WHERE departure='$departure' AND arrival='$arrival'", function($row) {
             return new Model\Route((string)$row->departure, (string)$row->arrival, (int)$row->first_class, (int)$row->business, (int)$row->economy);
         });
     }
@@ -94,9 +94,9 @@ class Database extends Object
     /**
      * Returns array of Routes or empty array if no Routes are found.
      */
-    public function getRoutes(): array
+    public static function getRoutes(): array
     {
-        return $this->queryMultiple("SELECT * FROM Route", function($row) {
+        return self::queryMultiple("SELECT * FROM Route", function($row) {
             return new Model\Route((string)$row->departure, (string)$row->arrival, (int)$row->first_class, (int)$row->business, (int)$row->economy);
         });
     }
@@ -104,9 +104,9 @@ class Database extends Object
     /**
      * Returns array of Flights or empty array if no Flights are found.
      */
-    public function getFlights(): array
+    public static function getFlights(): array
     {
-        return $this->queryMultiple("SELECT * FROM Flight", function($row) {
+        return self::queryMultiple("SELECT * FROM Flight", function($row) {
             $date = new \DateTime($row->date_time);
             $res = $date->format('h:i A, F d, Y');
             return new Model\Flight((int)$row->id, (string)$res, (string)$row->assigned, (string)$row->arrival, (string)$row->departure);
@@ -116,9 +116,9 @@ class Database extends Object
     /**
      * Returns a Flight with id or null if not found.
      */
-    public function getFlight(int $id): ?Model\Flight
+    public static function getFlight(int $id): ?Model\Flight
     {
-        return $this->querySingle("SELECT * FROM Flight WHERE id=$id", function($row) {
+        return self::querySingle("SELECT * FROM Flight WHERE id=$id", function($row) {
             $date = new \DateTime($row->date_time);
             $res = $date->format('h:i A, F d, Y');
             return new Model\Flight((int)$row->id, (string)$res, (string)$row->assigned, (string)$row->arrival, (string)$row->departure);
@@ -128,16 +128,16 @@ class Database extends Object
     /**
      * Returns array of Airports or empty array if no Airports are found.
      */
-    public function getAirports(): array
+    public static function getAirports(): array
     {
-        return $this->queryMultiple("SELECT * FROM Airport", function($row) {
+        return self::queryMultiple("SELECT * FROM Airport", function($row) {
             return new Model\Airport((string)$row->id, (string)$row->name, (string)$row->location);
         });
     }
     // General search of airports
-    public function getAirportsSearch(string $query): array // why can't I overload methods :(
+    public static function getAirportsSearch(string $query): array // why can't I overload methods :(
     {
-        return $this->queryMultiple("SELECT * FROM Airport
+        return self::queryMultiple("SELECT * FROM Airport
             WHERE (`id` LIKE '%".$query."%') OR (`name` LIKE '%".$query."%') OR (`location` LIKE '%".$query."%')",
             function($row){
                 return new Model\Airport((string)$row->id, (string)$row->name, (string)$row->location);
@@ -147,9 +147,9 @@ class Database extends Object
     /**
      * Returns array of Tickets or empty array if no Tickets are found.
      */
-    public function getTickets(): array
+    public static function getTickets(): array
     {
-        return $this->queryMultiple("SELECT * FROM Ticket", function($row) {
+        return self::queryMultiple("SELECT * FROM Ticket", function($row) {
             return new Model\Ticket((string)$row->id, (string)$row->seat_type, (string)$row->flightId,
                 (string)$row->customerId);
         });
@@ -158,27 +158,45 @@ class Database extends Object
     /**
      * Returns a Ticket with id or null if not found.
      */
-    public function getTicket(int $id): ?Model\Ticket
+    public static function getTicket(int $id): ?Model\Ticket
     {
-        return $this->querySingle("SELECT * FROM Ticket WHERE id=$id", function($row) {
+        return self::querySingle("SELECT * FROM Ticket WHERE id=$id", function($row) {
             return new Model\Ticket((string)$row->id, (string)$row->seat_type, (string)$row->flightId,
                 (string)$row->customerId);
         });
     }
 
+    private static function isAccount(string $query): bool {
+        try {
+            $result = self::$mysql->query($query);
+            Log::info("$query returned $result->num_rows rows");
+            if ($result->num_rows == 0) {
+                return false;
+            }
+            if ($result->num_rows > 1) {
+                throw new \Exception("Duplicate account detected.");
+            }
+            $result->close();
+            return true;
+        } catch (MySQLException $e) {
+            self::logSqlError($e);
+            return false;
+        }
+    }
+
     public function __destruct()
     {
         Log::info('Closing MySQL connection');
-        $this->mysql->close();
+        self::$mysql->close();
     }
 
-    private function logSqlError(MySQLException $e) {
+    private static function logSqlError(MySQLException $e) {
         Log::alert("Query failed with errno: ".$e->getCode()."\n".$e->getMessage());
     }
 
-    private function querySingle(string $query, callable $fn) {
+    private static function querySingle(string $query, callable $fn) {
         try {
-            $result = $this->mysql->query($query);
+            $result = self::$mysql->query($query);
             Log::info("$query returned $result->num_rows rows");
             if ($result->num_rows == 0) {
                 return null;
@@ -193,14 +211,14 @@ class Database extends Object
 
             return $model;
         } catch (MySQLException $e) {
-            $this->logSqlError($e);
+            self::logSqlError($e);
             return null;
         }
     }
 
-    private function queryMultiple(string $query, callable $fn) {
+    private static function queryMultiple(string $query, callable $fn) {
         try {
-            $result = $this->mysql->query($query);
+            $result = self::$mysql->query($query);
             Log::info("$query $result->num_rows rows");
             if ($result->num_rows == 0) {
                 return [];
@@ -214,26 +232,8 @@ class Database extends Object
 
             return $models;
         } catch (MySQLException $e) {
-            $this->logSqlError($e);
+            self::logSqlError($e);
             return [];
-        }
-    }
-
-    private function isAccount(string $query): bool {
-        try {
-            $result = $this->mysql->query($query);
-            Log::info("$query returned $result->num_rows rows");
-            if ($result->num_rows == 0) {
-                return false;
-            }
-            if ($result->num_rows > 1) {
-                throw new \Exception("Duplicate account detected.");
-            }
-            $result->close();
-            return true;
-        } catch (MySQLException $e) {
-            $this->logSqlError($e);
-            return false;
         }
     }
 }
