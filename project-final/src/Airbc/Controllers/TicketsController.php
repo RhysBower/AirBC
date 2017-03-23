a58bd58ff13500f19740933ea7e25f9bcccfd97f
harrysonhu <harrysonh95@hotmail.com>
2017-03-22 21:31:15 -0700

    Skeleton of adding tickets. Not fully working yet

--- project-final/src/Airbc/Controllers/TicketsController.php
+++ project-final/src/Airbc/Controllers/TicketsController.php
@@ -39,4 +39,34 @@ class TicketsController extends Controller
         echo $template->render($this->context);
     }
 
+    public function addTicket() 
+    {
+        $this->context['page'] = "tickets";
+        if (array_key_exists('from', $_POST) && $_POST['from'] !== "" &&
+            array_key_exists('to', $_POST) && $_POST['to'] !== "" &&
+            array_key_exists('passengers', $_POST) && $_POST['passengers'] !== "") {
+            $from = $_POST['from'];
+            $to = $_TO['to'];
+            $passengers = $_POST['passengers'];
+
+            $flights = $this->database->getFlights();
+            for ($x = 0; $x < sizeof($flights); $x++) {
+                if ($flights[$x].departure === $from && $flights[$x].arrival === $to) {
+                    $id = (string) $flights[$x].id;
+                    $this->database->addTicket($id);
+                } else {
+                    $this->context['error'] = "This is not a valid flight to book!";
+                }
+            }
+            
+            header('Location: /tickets');
+        } else {
+            // render error
+            $this->context['error'] = "Please fill in all the fields!";
+
+            $template = $this->twig->load('bookTicket.twig');
+            echo $template->render($this->context);
+        }
+    }
+
 }
