1d649575c29d3bc60b0e0a73daa054797305a885
harrysonhu <harrysonh95@hotmail.com>
2017-03-26 15:15:33 -0700

    Change addTicket to use queryModify. Move book button left.

--- project-final/src/Airbc/Database.php
+++ project-final/src/Airbc/Database.php
@@ -252,12 +252,9 @@ class Database extends Object
     /**
      * Adds a ticket
      */
-    public static function addTicket(string $flightId, string $seatType, string $customerId,                             string $accountId): array
+    public static function addTicket(string $flightId, string $seatType, string $customerId,                             string $accountId): bool
     {
-        Log::emergency('adding...');
-        return self::queryMultiple("INSERT INTO Ticket (seat_type, flightId, customerId, purchasedBy) VALUES ('$seatType', '$flightId', '$customerId', '$accountId')", function($row) {
-            return new Model\Ticket((string)$row->id, (string)$row->seat_type, (string)$row->flightId, (string)$row->customerId, (string)$row->purchasedBy);
-        });
+        return self::queryModify("INSERT INTO Ticket (seat_type, flightId, customerId, purchasedBy) VALUES ('$seatType', '$flightId', '$customerId', '$accountId')");
     }
 
     /**
